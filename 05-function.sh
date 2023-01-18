83eba2a95d158cc052e888dd0026e660a33df01d
Charan <gcharankumar99@gmail.com>
2023-01-18 08:49:22 -0600

    Update learn-shell

--- 05-function.sh
+++ 05-function.sh
@@ -1,12 +1,25 @@
-# Functions are like variables.
+# Functions are like variables
 # Variable - When we assign a name to a set of data then it is a variable.
-# Function - when we assign a name to a set of commands then it is a function.
+# Function - When we assign a name to a set of commands then it is function.
 
 
 # Function Declare
 example() {
   echo I am a example function
+  echo a - $a
+  b=20
 }
 
-## call a function
+## Call a function
+a=10
 example
+echo b - $b
+
+example1() {
+  echo example1 - abc
+  return 2
+  echo example1 - xyz
+}
+
+example1
+echo example1 return status - $?
